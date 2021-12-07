0b977ed6f95cb6e9b4533e0780c6d16b8621188f
The-Tychon <93827022+The-Tychon@users.noreply.github.com>
2021-12-08 01:57:08 +0530

    Update private.py

--- hellbot/plugins/private.py
+++ hellbot/plugins/private.py
@@ -1,4 +1,4 @@
-3from pyrogram import Client, filters
+from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 from config import BOT_NAME as bn
